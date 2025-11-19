5fa42e03af3fd974246a5fc7080f1c223854fa43
o1abdurakhimow <ibocheek777@gmail.com>
2025-11-19 18:05:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+let yosh = prompt("Yoshingiz nechida?");
+
+if (yosh >= 18) {
+    alert("Saytga kiririshingiz mumkin");
+} else {
+    alert("Siz hali kichkinsiz");
+}
+
+
+// let ism = prompt("Ismingiz nima?");
+// let date = prompt("Tug'ulgan yilingizni kiriting?");
+// let joriyyil = "2025"
+// let age = alert("Sizning yoshingiz " + (joriyyil - date) + " da");
+
+
+// console.log(ism);
+// console.log(age)
+
+
+
+// let obj = {
+//     name: null,
+//     age: null,
+//     id: null
+// }
+
+
+
+
+// let name = prompt("Ismingiz nima?");
+// let age = Number(prompt("Yoshingiz nechida?")) || 0;
+// let hasid;
+
+// let randomId = Math.floor(Math.random() * 100) + 1;
+// alert("Sizning random ID raqamingiz: " + randomId);
+
+// if (age >= 18) {
+//     alert("Xush kelibsiz");
+// } else {
+//     hasid = prompt("Id raqamingizni kiriting");
+// }
+
+
+
+// console.log(name);
+// console.log(age);
+// console.log(hasid);
+
+// obj.name = name;
+// obj.age = age;
+// obj.id = hasid;
+
+// console.log(obj);
